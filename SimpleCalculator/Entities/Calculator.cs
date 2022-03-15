95c8f630fb126179f2210ffa2d60888e5f1a8d9f
Douglas da Silva <douglas_wfs@hotmail.com>
2022-03-14 21:54:14 -0300

    First commit

---
+++ SimpleCalculator/Entities/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Entities
+{
+    public class Calculator
+    {       
+        public static double Sum(params double[] numbers)
+        {
+            double sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        public static double Subtraction(params double[] numbers)
+        {
+            double sub = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sub -= numbers[i];
+            }
+            return sub;
+        }
+        public static double Multiplication(params double[] numbers)
+        {
+            double mult = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                mult *= numbers[i];
+            }
+            return mult;
+        }
+        public static double Divison(params double[] numbers)
+        {
+
+            return numbers[0] / numbers[1];
+        }
+    }
+}
+
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using SimpleCalculator.Entities;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------CALCULADORA----------------");
+            Console.WriteLine();
+            Console.Write("Digite uma das quatro operações (+) , (-) , (*) ou (/): ");           
+            char sinal = char.Parse(Console.ReadLine());
+            if (sinal == '/')
+            {
+                Console.WriteLine("ATENÇÃO! Na divisão digite apenas 2 números!");
+            }
+            Console.Write("Quantos números você deseja calcular? ");           
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Digite o {i+1}º número: ");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (sinal == '+')
+            {
+                Console.WriteLine("O resultado é: " + Calculator.Sum(vect), CultureInfo.InvariantCulture);
+            }
+            else if(sinal == '-')
+            {
+                Console.WriteLine("O resultado é: " + Calculator.Subtraction(vect), CultureInfo.InvariantCulture);
+            }
+            else if (sinal == '*')
+            {
+                Console.WriteLine("O resultado é: " + Calculator.Multiplication(vect).ToString("F4", CultureInfo.InvariantCulture));
+            }
+            if (sinal == '/' && n == 2)
+            {
+                Console.WriteLine("O resultado é: " + Calculator.Divison(vect).ToString("F4", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida!");
+            }
+           
+            
+        }
+    }
+}
